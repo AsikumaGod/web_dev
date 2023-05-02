3900a9702806dc22693693fceaac8a9c01ce9e70
Stephen Adams <jacobantoine506@gmail.com>
2023-05-02 06:47:50 -0700

    Uploaded new project works
    Works Uploaded

---
+++ tictac/index.js
@@ -0,0 +1,19 @@
+var restart = document.querySelector('#res');
+var squares = document.querySelectorAll('td');
+function clearb(){
+    for (var i = 0; i < squares.length; i++)
+    squares[i].textContent = ''
+}
+restart.addEventListener('click', clearb);
+function changContent(){
+    if (this.textContent === ''){
+        this.textContent = 'X';
+    }else if (this.textContent === 'X'){
+        this.textContent = 'O';
+    }else{
+        this.textContent = '';
+    }
+}
+for (var i = 0; i < squares.length; i++){
+    squares[i].addEventListener('click',changContent)
+}
